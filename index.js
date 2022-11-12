b18a3500b07c213ab77b880b7408f27f0e9ae4df
LGocool <67392650+LGocool@users.noreply.github.com>
2022-11-12 12:32:07 -0500

    commit tree 4f27c0848bf2943e4ddfdd568add96018ee9a616

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+wrapGifts(gifts);
+
+function writeCards(people,event){
+  const person =[];
+    for (let i =0; i < people.length;i++) {
+       person.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
+  }
+   return person;
+}
+   console.log(writeCards(["Charlie","Samip","Ali"],"birthday"));
+
+function countDown(num){
+  let countdown = 0;
+     while (countdown <= num){
+       console.log(countdown++);
+       
+   }
+   }
+countDown(10);
